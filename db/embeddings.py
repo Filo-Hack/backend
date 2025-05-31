e2bdf0cadcae117362b03b178ac9f42164e20e2a
Aquil <nindzy142@yandex.ru>
2025-06-01 01:05:58 +0300

    make init db and manager for db

---
+++ db/embeddings.py
@@ -0,0 +1,8 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from config import settings
+
+embeddings = HuggingFaceEmbeddings(
+    model_name=settings.LM_MODEL_NAME,
+    model_kwargs={"device": "cuda"}  # или "cuda" если у тебя есть GPU
+)
+
